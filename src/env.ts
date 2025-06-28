600a3571341bbc9d1f8bb0f7b377d641e0db16ca
zuhayrali <31449576+zuhayrali@users.noreply.github.com>
2025-06-28 01:39:42 -0500

    feat: support docker secrets

--- src/env.ts
+++ src/env.ts
@@ -1,17 +1,29 @@
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
+
 dotenv.config();
 
+function readDockerSecret(key: string): string | undefined {
+  const secretPath = path.join("/run/secrets", key);
+  if (fs.existsSync(secretPath)) {
+    return fs.readFileSync(secretPath, "utf8").trim();
+  }
+  return undefined;
+}
+
 function getRequiredEnvVar(key: string): string {
-  const value = process.env[key];
+  const value = process.env[key] || readDockerSecret(key);
   if (!value) {
-    throw new Error(`Missing required environment variable: ${key}`);
+    throw new Error(`Missing required environment variable or secret: ${key}`);
   }
   return value;
 }
 
 function getOptionalEnvVar(key: string, defaultValue: string): string {
-  return process.env[key] ?? defaultValue;
+  return process.env[key] || readDockerSecret(key) || defaultValue;
 }
+
 
 export const env = {
   // Required
